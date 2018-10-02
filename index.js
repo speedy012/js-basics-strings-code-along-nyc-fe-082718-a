389801ecfad2c9daf227b87253098673a8a8f04b
Herman <hrivas5726@aol.com>
2018-10-02 10:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}`;
+
